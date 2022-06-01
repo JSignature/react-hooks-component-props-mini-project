c277a3104715268d9d08c68074d35a5c8d774525
Jimmy Andreas <jimmysignature@gmail.com>
2022-05-31 20:21:16 -0400

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,9 @@ import React from 'react'
 function About({ image = 'https://via.placeholder.com/215', aboutText }) {
   return (
     <aside>
-      <img src={image} alt="blog logo" />
-      <p>{aboutText}</p>
+      {/* <img src={image} alt="blog logo" />
+      <p>{aboutText}</p> */}
     </aside>
   )
 }
-
 export default About
